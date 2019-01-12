d77e68788b8d500c57c68d9f0cb964758099a2e4
Meo22 <kevin.michel@gmail.com>
2019-01-12 20:56:18 +0100

    Add Parse.cs file

---
+++ json/Parse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonUX.json
+{
+	public class Parse
+	{
+		/// <summary>
+		/// Parses a simple json file and returns values as string.
+		/// </summary>
+		/// <param name="json"></param>
+		/// <returns>A dictionary with string values</returns>
+		public Dictionary<string, string> ParseSimple(string json)
+		{
+			return ParseSimple(json, null);
+		}
+
+		/// <summary>
+		/// Parses a simple json file and returns values as string. If variables are in sub-level, <paramref name="entryPoint"/> can be specified. Parent properties are ignored.
+		/// </summary>
+		/// <param name="json"></param>
+		/// <returns>A dictionary with string values</returns>
+		public Dictionary<string, string> ParseSimple(string json, string entryPoint)
+		{
+			return new Dictionary<string, string>();
+		}
+	}
+}

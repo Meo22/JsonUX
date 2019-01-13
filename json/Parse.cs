c9fb97bd5e1fdcd886934c2259ce8dc3dcb11fde
Meo22 <kevin.michel@gmail.com>
2019-01-13 01:22:05 +0100

    Implemented ParseSimple

--- json/Parse.cs
+++ json/Parse.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JsonUX.json
+namespace JsonUX.Json
 {
 	public class Parse
 	{
@@ -13,19 +15,32 @@ namespace JsonUX.json
 		/// </summary>
 		/// <param name="json"></param>
 		/// <returns>A dictionary with string values</returns>
-		public Dictionary<string, string> ParseSimple(string json)
-		{
-			return ParseSimple(json, null);
-		}
+		public static Dictionary<string, string> ParseSimple(string json) => ParseSimple(json, null);
 
 		/// <summary>
-		/// Parses a simple json file and returns values as string. If variables are in sub-level, <paramref name="entryPoint"/> can be specified. Parent properties are ignored.
+		/// Parses a simple json file and returns values as string. If variables are in sub-level, <paramref name="entryPoint"/> can be specified.
 		/// </summary>
 		/// <param name="json"></param>
+		/// <param name="entryPoint"></param>
 		/// <returns>A dictionary with string values</returns>
-		public Dictionary<string, string> ParseSimple(string json, string entryPoint)
+		public static Dictionary<string, string> ParseSimple(string json, string entryPoint)
 		{
-			return new Dictionary<string, string>();
+			if(!string.IsNullOrEmpty(entryPoint))
+			{
+				var _entryPoints = entryPoint.Split('/');
+
+				var parsedJson = JObject.Parse(json);
+				
+				foreach (var _entryPoint in _entryPoints)
+				{
+					parsedJson = (JObject)parsedJson[entryPoint];
+				}
+
+				var results = parsedJson.Children();
+				json = JsonConvert.SerializeObject(results);
+			}
+
+			return JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
 		}
 	}
 }
